835aa59499c7e9a3249a914d4c769989c216a16c
Vesperpiano <barry.j.a.smart@gmail.com>
2019-09-27 18:23:55 +0100

    Made option to blank zeros in print an option.

--- Exercise2Triangle/TestScript.py
+++ Exercise2Triangle/TestScript.py
@@ -1,18 +1,22 @@
 from TriangleFunctions import *
 
-triangle_list_from_file = read_triangle_from_file("c:/Users/Barry/Documents/GitHub/CS814/Exercise2Triangle/", "triangle_numbers_2.txt")
+triangle_list_from_file = read_triangle_from_file("c:/Users/Barry/Documents/GitHub/CS814/Exercise2Triangle/", "triangle_numbers_1.txt")
 
 rolled_up_sum_triangle, solution_triangle, max_sum = roll_up_triangle_from_bottom(triangle_list_from_file)
 
 solution_path, source_triangle_masked = build_solution(triangle_list_from_file, solution_triangle)
 
-#print_triangle(triangle_list_from_file, False)
+print_triangle(triangle_list_from_file, False, False)
 
-#print_triangle(rolled_up_sum_triangle, False)
+print_triangle(rolled_up_sum_triangle, False, False)
 
-print_triangle(source_triangle_masked, True)
+print_triangle(solution_triangle, False, False)
 
-print('--- Maximum sum:', rolled_up_sum_triangle[0][0])
-print('--- Solution:')
+print_triangle(source_triangle_masked, False, True)
+
+print_triangle(source_triangle_masked, True, True)
+
+print('Maximum sum:', max_sum)
+print('Solution:')
 for i in range (0, len(solution_path)):
     print(solution_path[i])
