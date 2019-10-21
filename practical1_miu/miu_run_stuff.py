521bb0779f71f45aeb72bcbf4b8973befb5738e3
Vesperpiano <barry.j.a.smart@gmail.com>
2019-10-21 17:23:22 +0100

    Demo of debugging.

--- practical1_miu/miu_run_stuff.py
+++ practical1_miu/miu_run_stuff.py
@@ -1,3 +1,4 @@
+from miu_next_states import next_states
 from miu_extend_path import extend_path
 from miu_breadth_first_search import breadth_first_search
 from miu_depth_limited_dfs import depth_limited_dfs
@@ -9,4 +10,6 @@ from miu_iterative_deepening import iterative_deepening
 
 # depth_limited_dfs("MUI", 2)
 
-iterative_deepening("MUI")
+#iterative_deepening("MUI")
+
+next_states("MIIIIIIII")
