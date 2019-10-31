f9b5d5199be8ac65f25e72a7fe37698282e724aa
Vesperpiano <barry.j.a.smart@gmail.com>
2019-10-31 20:33:27 +0000

    Added heading to notebook.

--- practical1_miu_part5/miu2_notebook.py
+++ practical1_miu_part5/miu2_notebook.py
@@ -1,3 +1,11 @@
+#%% [markdown]
+## University of Strathclyde - MSc Artificial Intelligence and Applications
+## CS814 - Artificial Intelligence for Autonomous Systems
+## Assignment 1 - MIU
+# This notebook was first created first created 4th October 2019 by Barry Smart.
+#
+
+
 #%%
 import pandas as pd
 import numpy as np
