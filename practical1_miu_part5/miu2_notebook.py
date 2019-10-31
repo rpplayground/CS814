b91f61e8a25b6d6c078d9907bf91ea3f2be61cc4
Vesperpiano <barry.j.a.smart@gmail.com>
2019-10-31 20:10:58 +0000

    Changes to run in IDLE.

--- practical1_miu_part5/miu2_notebook.py
+++ practical1_miu_part5/miu2_notebook.py
@@ -4,16 +4,12 @@ import numpy as np
 from miu2_main import iterate_through_goals
 
 #%%
-list_of_goals = [ "MIU", "MIIII", "MUI", "MIIIIUIIIIU", "MIUUIIIIU",\
-     "MIUUIUU", "MIUUIUUIUUIUU", "MIUUIUUIIUU", "MIUUIUUII" ]
-
-#%%
-list_of_goals = [ "MIU", "MIIII", "MUI" ]
+list_of_goals = [ "MIU", "MIIII", "MUI", "MIIIIUIIIIU", "MIUUIIIIU", "MIUUIUU", "MIUUIUUIUUIUU", "MIUUIUUIIUU", "MIUUIUUII", "MIUUIUUIIU" ]
 
 #%%
 results_dataframe = iterate_through_goals(list_of_goals)
 
 # %%
-results_dataframe
+print(results_dataframe)
 
 #%%
